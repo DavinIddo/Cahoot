69aa859389b26474b7605404145a753185605aaf
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2021-07-29 08:07:47 +1000

    Create a simple header

---
+++ src/component/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="App-Header">
+                <p>This is the header of the app</p>
+            </header>
+        );
+    }
+}
+
+export default Header;
