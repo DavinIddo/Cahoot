0ed991f8ba8d3d9733c6e1d6d74b4b3abce1cd81
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2021-12-20 12:24:44 +1000

    Connect to MongoDB and perform CRD testing

--- BE/app/__init__.py
+++ BE/app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from .todos import todos
+from .extension import mongo
 
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb+srv://admin:admin123@cluster-condusk.l2mgi.mongodb.net/conduskdb?retryWrites=true&w=majority"
+
+mongo.init_app(app)
 
 app.register_blueprint(todos)
--- BE/app/todos.py
+++ BE/app/todos.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, jsonify, render_template
+from bson import json_util
+from .extension import mongo
 
 todos = Blueprint('todos', __name__)
 
@@ -9,7 +11,19 @@ def home():
 @todos.route("/submit_todo", methods=['POST'])
 def submit_todo():
     if request.method == 'POST':
-        return jsonify({'ok': True, 'message': 'True request method'}), 200
+        data = request.get_json()
+        data["completed"] = False
+
+        mongo.db.todos.insert_one(data)
+        mongo.db.todos.delete_one({'_id': data['_id']})
+        
+        all_result = mongo.db.todos.find()
+        all_todos = json_util.loads(json_util.dumps(all_result))
+
+        for i in all_todos: i['_id'] = str(i['_id'])
+
+        # return jsonify({'ok': True, 'message': 'True request method'}), 200
+        return jsonify({'ok': True, 'message': 'True request method', 'todos_list': all_todos}), 200
 
     return jsonify({'ok': False, 'message': 'False request method'}), 400
 
